7a1cf7a20550dd175d40327434b20d86f98d2f91
g1n0st <g1n0st@live.com>
2020-03-07 23:18:47 +0800

    Add OpenGL extension feature support and GUIRenderer framework

---
+++ src/GUI/ayagui.cpp
@@ -0,0 +1,271 @@
+#include "ayagui.h"
+
+#include <exception>
+#include <functional>
+
+namespace Aya {
+	PFNGLCREATESHADERPROC							glCreateShader;
+	PFNGLCREATEPROGRAMPROC							glCreateProgram;
+	PFNGLDELETESHADERPROC							glDeleteShader;
+	PFNGLDELETEPROGRAMPROC							glDeleteProgram;
+	PFNGLATTACHSHADERPROC							glAttachShader;
+	PFNGLDETACHSHADERPROC							glDetachShader;
+	PFNGLSHADERSOURCEPROC							glShaderSource;
+	PFNGLCOMPILESHADERPROC							glCompileShader;
+	PFNGLLINKPROGRAMPROC							glLinkProgram;
+	PFNGLVALIDATEPROGRAMPROC						glValidateProgram;
+	PFNGLUSEPROGRAMPROC								glUseProgram;
+	PFNGLGETSHADERINFOLOGPROC						glGetShaderInfoLog;
+	PFNGLGETPROGRAMINFOLOGPROC						glGetProgramInfoLog;
+	PFNGLGETSHADERIVPROC							glGetShaderiv;
+	PFNGLGETPROGRAMIVPROC							glGetProgramiv;
+	PFNGLUNIFORM1FPROC								glUniform1f;
+	PFNGLUNIFORM1FVPROC								glUniform1fv;
+	PFNGLUNIFORM2FPROC								glUniform2f;
+	PFNGLUNIFORM2FVPROC								glUniform2fv;
+	PFNGLUNIFORM3FPROC								glUniform3f;
+	PFNGLUNIFORM4FPROC								glUniform4f;
+	PFNGLUNIFORM1IPROC								glUniform1i;
+	PFNGLUNIFORM1IVPROC								glUniform1iv;
+	PFNGLUNIFORM2IPROC								glUniform2i;
+	PFNGLUNIFORM3IPROC								glUniform3i;
+	PFNGLUNIFORM4IPROC								glUniform4i;
+	PFNGLUNIFORMMATRIX3FVPROC						glUniformMatrix3fv;
+	PFNGLUNIFORMMATRIX4FVPROC						glUniformMatrix4fv;
+	PFNGLGETUNIFORMLOCATIONPROC						glGetUniformLocation;
+	PFNGLBINDFRAGDATALOCATIONEXTPROC				glBindFragDataLocation;
+	PFNGLISRENDERBUFFEREXTPROC						glIsRenderbuffer;
+	PFNGLBINDRENDERBUFFEREXTPROC					glBindRenderbuffer;
+	PFNGLDELETERENDERBUFFERSEXTPROC					glDeleteRenderbuffers;
+	PFNGLGENRENDERBUFFERSEXTPROC					glGenRenderbuffers;
+	PFNGLRENDERBUFFERSTORAGEEXTPROC					glRenderbufferStorage;
+	PFNGLRENDERBUFFERSTORAGEMULTISAMPLEEXTPROC		glRenderbufferStorageMultisample;
+	PFNGLGETRENDERBUFFERPARAMETERIVEXTPROC			glGetRenderbufferParameteriv;
+	PFNGLISFRAMEBUFFEREXTPROC						glIsFramebuffer;
+	PFNGLBINDFRAMEBUFFEREXTPROC						glBindFramebuffer;
+	PFNGLDELETEFRAMEBUFFERSEXTPROC					glDeleteFramebuffers;
+	PFNGLGENFRAMEBUFFERSEXTPROC						glGenFramebuffers;
+	PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC				glCheckFramebufferStatus;
+	PFNGLFRAMEBUFFERTEXTURE1DEXTPROC				glFramebufferTexture1D;
+	PFNGLFRAMEBUFFERTEXTURE2DEXTPROC				glFramebufferTexture2D;
+	PFNGLFRAMEBUFFERTEXTURE3DEXTPROC				glFramebufferTexture3D;
+	PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC				glFramebufferRenderbuffer;
+	PFNGLGETFRAMEBUFFERATTACHMENTPARAMETERIVEXTPROC	glGetFramebufferAttachmentParameteriv;
+	PFNGLBLITFRAMEBUFFEREXTPROC						glBlitFramebuffer;
+	PFNGLGENERATEMIPMAPEXTPROC						glGenerateMipmap;
+	PFNGLDRAWBUFFERSPROC							glDrawBuffers;
+	PFNGLDRAWRANGEELEMENTSPROC						glDrawRangeElements;
+	PFNGLACTIVETEXTUREPROC							glActiveTexture;
+	PFNGLCLIENTACTIVETEXTUREPROC					glClientActiveTexture;
+	PFNGLMULTITEXCOORD1FPROC						glMultiTexcoord1f;
+	PFNGLMULTITEXCOORD2FPROC						glMultiTexcoord2f;
+	PFNGLMULTITEXCOORD3FPROC						glMultiTexcoord3f;
+	PFNGLMULTITEXCOORD4FPROC						glMultiTexcoord4f;
+	PFNGLLOADTRANSPOSEMATRIXFPROC					glLoadTransposeMatrixf;
+	PFNGLBINDBUFFERPROC								glBindBuffer;
+	PFNGLBUFFERDATAPROC								glBufferData;
+	PFNGLBUFFERSUBDATAPROC							glBufferSubData;
+	PFNGLDELETEBUFFERSPROC							glDeleteBuffers;
+	PFNGLGENBUFFERSPROC								glGenBuffers;
+	PFNGLMAPBUFFERPROC								glMapBuffer;
+	PFNGLUNMAPBUFFERPROC							glUnmapBuffer;
+	PFNGLBLENDEQUATIONPROC							glBlendEquation;
+	PFNGLBLENDCOLORPROC								glBlendColor;
+	PFNGLBLENDFUNCSEPARATEPROC						glBlendFuncSeperate;
+	PFNGLBLENDEQUATIONSEPARATEPROC					glBlendEquationSeperate;
+
+	void InitializeOpenGLExtensions() {
+		glCreateShader = (PFNGLCREATESHADEROBJECTARBPROC)wglGetProcAddress("glCreateShaderObjectARB");
+		glCreateProgram = (PFNGLCREATEPROGRAMOBJECTARBPROC)wglGetProcAddress("glCreateProgramObjectARB");
+		glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
+		glDetachShader = (PFNGLDETACHSHADERPROC)wglGetProcAddress("glDetachShader");
+		glDeleteShader = (PFNGLDELETESHADERPROC)wglGetProcAddress("glDeleteShader");
+		glDeleteProgram = (PFNGLDELETEPROGRAMPROC)wglGetProcAddress("glDeleteProgram");
+		glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
+		glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
+		glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
+		glValidateProgram = (PFNGLVALIDATEPROGRAMPROC)wglGetProcAddress("glValidateProgram");
+		glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
+		glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
+		glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)wglGetProcAddress("glGetProgramInfoLog");
+		glGetShaderiv = (PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv");
+		glGetProgramiv = (PFNGLGETPROGRAMIVPROC)wglGetProcAddress("glGetProgramiv");
+		glUniform1f = (PFNGLUNIFORM1FPROC)wglGetProcAddress("glUniform1f");
+		glUniform1fv = (PFNGLUNIFORM1FVPROC)wglGetProcAddress("glUniform1fv");
+		glUniform2f = (PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f");
+		glUniform2fv = (PFNGLUNIFORM2FVPROC)wglGetProcAddress("glUniform2fv");
+		glUniform3f = (PFNGLUNIFORM3FPROC)wglGetProcAddress("glUniform3f");
+		glUniform4f = (PFNGLUNIFORM4FPROC)wglGetProcAddress("glUniform4f");
+		glUniform1i = (PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i");
+		glUniform1iv = (PFNGLUNIFORM1IVPROC)wglGetProcAddress("glUniform1iv");
+		glUniform2i = (PFNGLUNIFORM2IPROC)wglGetProcAddress("glUniform2i");
+		glUniform3i = (PFNGLUNIFORM3IPROC)wglGetProcAddress("glUniform3i");
+		glUniform4i = (PFNGLUNIFORM4IPROC)wglGetProcAddress("glUniform4i");
+		glUniformMatrix3fv = (PFNGLUNIFORMMATRIX3FVPROC)wglGetProcAddress("glUniformMatrix3fv");
+		glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)wglGetProcAddress("glUniformMatrix4fv");
+		glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
+		glBindFragDataLocation = (PFNGLBINDFRAGDATALOCATIONEXTPROC)wglGetProcAddress("glBindFragDataLocationEXT");
+
+		glIsRenderbuffer = (PFNGLISRENDERBUFFEREXTPROC)wglGetProcAddress("glIsRenderbuffer");
+		glBindRenderbuffer = (PFNGLBINDRENDERBUFFEREXTPROC)wglGetProcAddress("glBindRenderbuffer");
+		glDeleteRenderbuffers = (PFNGLDELETERENDERBUFFERSEXTPROC)wglGetProcAddress("glDeleteRenderbuffers");
+		glGenRenderbuffers = (PFNGLGENRENDERBUFFERSEXTPROC)wglGetProcAddress("glGenRenderbuffers");
+		glRenderbufferStorage = (PFNGLRENDERBUFFERSTORAGEEXTPROC)wglGetProcAddress("glRenderbufferStorage");
+		glGetRenderbufferParameteriv = (PFNGLGETRENDERBUFFERPARAMETERIVEXTPROC)wglGetProcAddress("glGetRenderbufferParameteriv");
+		glIsFramebuffer = (PFNGLISFRAMEBUFFEREXTPROC)wglGetProcAddress("glIsFramebuffer");
+		glBindFramebuffer = (PFNGLBINDFRAMEBUFFEREXTPROC)wglGetProcAddress("glBindFramebuffer");
+		glDeleteFramebuffers = (PFNGLDELETEFRAMEBUFFERSEXTPROC)wglGetProcAddress("glDeleteFramebuffers");
+		glGenFramebuffers = (PFNGLGENFRAMEBUFFERSEXTPROC)wglGetProcAddress("glGenFramebuffers");
+		glCheckFramebufferStatus = (PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC)wglGetProcAddress("glCheckFramebufferStatus");
+		glFramebufferTexture1D = (PFNGLFRAMEBUFFERTEXTURE1DEXTPROC)wglGetProcAddress("glFramebufferTexture1D");
+		glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2DEXTPROC)wglGetProcAddress("glFramebufferTexture2D");
+		glFramebufferTexture3D = (PFNGLFRAMEBUFFERTEXTURE3DEXTPROC)wglGetProcAddress("glFramebufferTexture3D");
+		glFramebufferRenderbuffer = (PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC)wglGetProcAddress("glFramebufferRenderbuffer");
+		glGetFramebufferAttachmentParameteriv = (PFNGLGETFRAMEBUFFERATTACHMENTPARAMETERIVEXTPROC)wglGetProcAddress("glGetFramebufferAttachmentParameteriv");
+		glBlitFramebuffer = (PFNGLBLITFRAMEBUFFEREXTPROC)wglGetProcAddress("glBlitFramebuffer");
+		glRenderbufferStorageMultisample = (PFNGLRENDERBUFFERSTORAGEMULTISAMPLEEXTPROC)wglGetProcAddress("glRenderbufferStorageMultisample");
+		glGenerateMipmap = (PFNGLGENERATEMIPMAPEXTPROC)wglGetProcAddress("glGenerateMipmap");
+		glDrawBuffers = (PFNGLDRAWBUFFERSPROC)wglGetProcAddress("glDrawBuffers");
+		glDrawRangeElements = (PFNGLDRAWRANGEELEMENTSPROC)wglGetProcAddress("glDrawRangeElements");
+
+		glActiveTexture = (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
+		glClientActiveTexture = (PFNGLCLIENTACTIVETEXTUREPROC)wglGetProcAddress("glClientActiveTexture");
+		glMultiTexcoord1f = (PFNGLMULTITEXCOORD1FPROC)wglGetProcAddress("glMultiTexCoord1f");
+		glMultiTexcoord2f = (PFNGLMULTITEXCOORD2FPROC)wglGetProcAddress("glMultiTexCoord2f");
+		glMultiTexcoord3f = (PFNGLMULTITEXCOORD3FPROC)wglGetProcAddress("glMultiTexCoord3f");
+		glMultiTexcoord4f = (PFNGLMULTITEXCOORD4FPROC)wglGetProcAddress("glMultiTexCoord4f");
+		glLoadTransposeMatrixf = (PFNGLLOADTRANSPOSEMATRIXFPROC)wglGetProcAddress("glLoadTransposeMatrixf");
+
+		glBindBuffer = (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
+		glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
+		glDeleteBuffers = (PFNGLDELETEBUFFERSPROC)wglGetProcAddress("glDeleteBuffers");
+		glGenBuffers = (PFNGLGENBUFFERSPROC)wglGetProcAddress("glGenBuffers");
+
+		glBlendEquation = (PFNGLBLENDEQUATIONPROC)wglGetProcAddress("glBlendEquation");
+		glBlendColor = (PFNGLBLENDCOLORPROC)wglGetProcAddress("glBlendColor");
+		glBlendFuncSeperate = (PFNGLBLENDFUNCSEPARATEPROC)wglGetProcAddress("glBlendFuncSeperate");
+		glBlendEquationSeperate = (PFNGLBLENDEQUATIONSEPARATEPROC)wglGetProcAddress("glBlendEquationSeperate");
+	}
+
+	GUIRenderer* GUIRenderer::mp_instance = nullptr;
+
+	const GLfloat GUIRenderer::DEPTH_FAR = .8f;
+	const GLfloat GUIRenderer::DEPTH_MID = .6f;
+	const GLfloat GUIRenderer::DEPTH_NEAR = .4f;
+
+	const GLchar* GUIRenderer::vert_shader_source = R"(
+		    varying vec2 texCoord;
+			void main()
+			{
+				gl_Position = gl_Vertex;
+				texCoord = gl_MultiTexCoord0.xy;
+			})";
+	const GLchar* GUIRenderer::blur_frag_shader_source = R"(
+			uniform sampler2D texSampler;
+			uniform float weights[13];
+			uniform vec2 offsets[13];
+			varying vec2 texCoord;
+			void main()
+			{
+				vec4 sample = 0.0f;
+				for(int i = 0; i < 13; i++)
+				{
+					sample += weights[i] * texture2DLod(texSampler, texCoord + offsets[i], 2);
+				}
+				gl_FragColor = vec4(sample.rgb, 1.0);
+			})";
+
+	GUIRenderer::GUIRenderer() {
+		m_handle_program = glCreateProgram();
+
+		glGenTextures(1, &m_handle_bg_tex);
+		glBindTexture(GL_TEXTURE_2D, m_handle_bg_tex);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
+
+		glGenFramebuffers(1, &m_handle_FBO);
+		glGenRenderbuffers(1, &m_handle_color_RBO);
+
+		m_tex_list_base = glGenLists(128);
+		HFONT font = CreateFont(16,
+			0,
+			0,
+			0,
+			FW_BOLD,
+			FALSE,
+			FALSE,
+			FALSE,
+			DEFAULT_CHARSET,
+			OUT_TT_PRECIS,
+			CLIP_DEFAULT_PRECIS,
+			ANTIALIASED_QUALITY,
+			FF_DECORATIVE | DEFAULT_PITCH,
+			"Helvetica");
+		m_HDC = GetDC(0);
+		m_font = (HFONT)SelectObject(m_HDC, font);
+		wglUseFontBitmaps(m_HDC, 0, 128, m_tex_list_base);
+		SelectObject(m_HDC, m_font);
+		DeleteObject(font);
+
+		auto loadShader = [](GLint type, const GLchar *source) {
+			GLint handle = glCreateShader(type);
+			GLint length = (GLint)strlen(source);
+			glShaderSource(handle, 1, &source, &length);
+			glCompileShader(handle);
+
+			GLint compile_status;
+			glGetShaderiv(handle, GL_COMPILE_STATUS, &compile_status);
+			if (compile_status != GL_TRUE)
+				throw std::exception("GLSL compiled error");
+
+			return handle;
+		};
+
+		GLint handle_vertex_shader = loadShader(GL_VERTEX_SHADER, vert_shader_source);
+		GLint handle_blur_frag_shader = loadShader(GL_FRAGMENT_SHADER, blur_frag_shader_source);
+		glAttachShader(m_handle_program, handle_vertex_shader);
+		glAttachShader(m_handle_program, handle_blur_frag_shader);
+
+		
+		auto excuteAndCheck = [](std::function<void(GLint)> glFunc, GLint handle) {
+			GLint status;
+			GLchar* status_buffer;
+			GLint length;
+
+			glFunc(handle);
+			glGetProgramiv(handle, GL_INFO_LOG_LENGTH, &length);
+			status_buffer = new GLchar[length];
+			glGetProgramInfoLog(handle, length, &length, status_buffer);
+			glGetProgramiv(handle, GL_LINK_STATUS, &status);
+			if (status != GL_TRUE)
+				throw std::exception(status_buffer);
+
+			delete[] status_buffer;
+		};
+		
+		excuteAndCheck(glLinkProgram, m_handle_program);
+		excuteAndCheck(glValidateProgram, m_handle_program);
+
+		// Calculate circle coordinates
+		const GLfloat phi_itvl = 0.57119866428905331608411697877809f;
+		GLfloat phi = 0.0f;
+		for (int i = 0; i < CIRCLE_VERTEX_COUNT - 1; ++i) {
+			m_circle_coords[i * 2] = sin(phi);
+			m_circle_coords[i * 2 + 1] = cos(phi);
+			phi += phi_itvl;
+		}
+		m_circle_coords[2 * CIRCLE_VERTEX_COUNT - 2] = m_circle_coords[0];
+		m_circle_coords[2 * CIRCLE_VERTEX_COUNT - 1] = m_circle_coords[1];
+	}
+
+	GUIRenderer::~GUIRenderer() {
+		glDeleteProgram(m_handle_program);
+		glDeleteTextures(1, &m_handle_bg_tex);
+		glDeleteFramebuffers(1, &m_handle_FBO);
+		glDeleteRenderbuffers(1, &m_handle_color_RBO);
+	}
+}
---
+++ src/GUI/ayagui.h
@@ -0,0 +1,145 @@
+#ifndef AYA_GUI_H
+#define AYA_GUI_H
+
+#include <math.h>
+
+#include <Windows.h>
+#include <minwindef.h>
+
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include "glext.h"
+
+namespace Aya {
+	// OpenGL Extension Loader
+	extern PFNGLCREATESHADERPROC							glCreateShader;
+	extern PFNGLCREATEPROGRAMPROC							glCreateProgram;
+	extern PFNGLDELETESHADERPROC							glDeleteShader;
+	extern PFNGLDELETEPROGRAMPROC							glDeleteProgram;
+	extern PFNGLATTACHSHADERPROC							glAttachShader;
+	extern PFNGLDETACHSHADERPROC							glDetachShader;
+	extern PFNGLSHADERSOURCEPROC							glShaderSource;
+	extern PFNGLCOMPILESHADERPROC							glCompileShader;
+	extern PFNGLLINKPROGRAMPROC								glLinkProgram;
+	extern PFNGLVALIDATEPROGRAMPROC							glValidateProgram;
+	extern PFNGLUSEPROGRAMPROC								glUseProgram;
+	extern PFNGLGETSHADERINFOLOGPROC						glGetShaderInfoLog;
+	extern PFNGLGETPROGRAMINFOLOGPROC						glGetProgramInfoLog;
+	extern PFNGLGETSHADERIVPROC								glGetShaderiv;
+	extern PFNGLGETPROGRAMIVPROC							glGetProgramiv;
+	extern PFNGLUNIFORM1FPROC								glUniform1f;
+	extern PFNGLUNIFORM1FVPROC								glUniform1fv;
+	extern PFNGLUNIFORM2FPROC								glUniform2f;
+	extern PFNGLUNIFORM2FVPROC								glUniform2fv;
+	extern PFNGLUNIFORM3FPROC								glUniform3f;
+	extern PFNGLUNIFORM4FPROC								glUniform4f;
+	extern PFNGLUNIFORM1IPROC								glUniform1i;
+	extern PFNGLUNIFORM1IVPROC								glUniform1iv;
+	extern PFNGLUNIFORM2IPROC								glUniform2i;
+	extern PFNGLUNIFORM3IPROC								glUniform3i;
+	extern PFNGLUNIFORM4IPROC								glUniform4i;
+	extern PFNGLUNIFORMMATRIX3FVPROC						glUniformMatrix3fv;
+	extern PFNGLUNIFORMMATRIX4FVPROC						glUniformMatrix4fv;
+	extern PFNGLGETUNIFORMLOCATIONPROC						glGetUniformLocation;
+	extern PFNGLBINDFRAGDATALOCATIONEXTPROC					glBindFragDataLocation;
+	extern PFNGLISRENDERBUFFEREXTPROC						glIsRenderbuffer;
+	extern PFNGLBINDRENDERBUFFEREXTPROC						glBindRenderbuffer;
+	extern PFNGLDELETERENDERBUFFERSEXTPROC					glDeleteRenderbuffers;
+	extern PFNGLGENRENDERBUFFERSEXTPROC						glGenRenderbuffers;
+	extern PFNGLRENDERBUFFERSTORAGEEXTPROC					glRenderbufferStorage;
+	extern PFNGLRENDERBUFFERSTORAGEMULTISAMPLEEXTPROC		glRenderbufferStorageMultisample;
+	extern PFNGLGETRENDERBUFFERPARAMETERIVEXTPROC			glGetRenderbufferParameteriv;
+	extern PFNGLISFRAMEBUFFEREXTPROC						glIsFramebuffer;
+	extern PFNGLBINDFRAMEBUFFEREXTPROC						glBindFramebuffer;
+	extern PFNGLDELETEFRAMEBUFFERSEXTPROC					glDeleteFramebuffers;
+	extern PFNGLGENFRAMEBUFFERSEXTPROC						glGenFramebuffers;
+	extern PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC				glCheckFramebufferStatus;
+	extern PFNGLFRAMEBUFFERTEXTURE1DEXTPROC					glFramebufferTexture1D;
+	extern PFNGLFRAMEBUFFERTEXTURE2DEXTPROC					glFramebufferTexture2D;
+	extern PFNGLFRAMEBUFFERTEXTURE3DEXTPROC					glFramebufferTexture3D;
+	extern PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC				glFramebufferRenderbuffer;
+	extern PFNGLGETFRAMEBUFFERATTACHMENTPARAMETERIVEXTPROC	glGetFramebufferAttachmentParameteriv;
+	extern PFNGLBLITFRAMEBUFFEREXTPROC						glBlitFramebuffer;
+	extern PFNGLGENERATEMIPMAPEXTPROC						glGenerateMipmap;
+	extern PFNGLDRAWBUFFERSPROC								glDrawBuffers;
+	extern PFNGLDRAWRANGEELEMENTSPROC						glDrawRangeElements;
+	extern PFNGLACTIVETEXTUREPROC							glActiveTexture;
+	extern PFNGLCLIENTACTIVETEXTUREPROC						glClientActiveTexture;
+	extern PFNGLMULTITEXCOORD1FPROC							glMultiTexcoord1f;
+	extern PFNGLMULTITEXCOORD2FPROC							glMultiTexcoord2f;
+	extern PFNGLMULTITEXCOORD3FPROC							glMultiTexcoord3f;
+	extern PFNGLMULTITEXCOORD4FPROC							glMultiTexcoord4f;
+	extern PFNGLLOADTRANSPOSEMATRIXFPROC					glLoadTransposeMatrixf;
+	extern PFNGLBINDBUFFERPROC								glBindBuffer;
+	extern PFNGLBUFFERDATAPROC								glBufferData;
+	extern PFNGLBUFFERSUBDATAPROC							glBufferSubData;
+	extern PFNGLDELETEBUFFERSPROC							glDeleteBuffers;
+	extern PFNGLGENBUFFERSPROC								glGenBuffers;
+	extern PFNGLMAPBUFFERPROC								glMapBuffer;
+	extern PFNGLUNMAPBUFFERPROC								glUnmapBuffer;
+	extern PFNGLBLENDEQUATIONPROC							glBlendEquation;
+	extern PFNGLBLENDCOLORPROC								glBlendColor;
+	extern PFNGLBLENDFUNCSEPARATEPROC						glBlendFuncSeperate;
+	extern PFNGLBLENDEQUATIONSEPARATEPROC					glBlendEquationSeperate;
+
+	void InitializeOpenGLExtensions();
+
+	class GUIRenderer {
+	private:
+		static GUIRenderer *mp_instance;
+
+		GLint m_tex_list_base;
+		GLint m_width, m_height;
+
+		// Font style render related
+		HFONT m_font;
+		HDC m_HDC;
+
+		GLuint m_handle_program;
+		GLuint m_handle_bg_tex;
+		GLuint m_handle_FBO;
+		GLuint m_handle_color_RBO;
+
+		// Gaussian blur weights and offsets
+		static const int BLUR_SAMPLE = 13;
+		GLfloat m_gaussian_weights[BLUR_SAMPLE];
+		GLfloat m_gaussian_offsets[BLUR_SAMPLE * 2];
+
+		// Precomputed coordinates for circle
+		static const int CIRCLE_VERTEX_COUNT = 12;
+		GLfloat m_circle_coords[CIRCLE_VERTEX_COUNT];
+
+		static const GLchar *vert_shader_source;
+		static const GLchar *blur_frag_shader_source;
+
+	public:
+		static const GLfloat DEPTH_FAR;
+		static const GLfloat DEPTH_MID;
+		static const GLfloat DEPTH_NEAR;
+
+	private:
+		GUIRenderer();
+		~GUIRenderer();
+
+	public:
+		static __forceinline GUIRenderer* instace() {
+			if (!mp_instance)
+				mp_instance = new GUIRenderer;
+			return mp_instance;
+		}
+		static void deleteInstance() {
+			if (!mp_instance) {
+				delete mp_instance;
+				mp_instance = nullptr;
+			}
+		}
+
+		__forceinline HDC getHDC() const {
+			return m_HDC;
+		}
+
+	};
+}
+
+#endif
